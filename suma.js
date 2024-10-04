d6bcf8e04bc4ce888b71494a7a9926cfac2092ad
Ronny De Los Santos <delossantosr835@gmail.com>
2024-10-04 00:27:59 -0400

    Mejora

--- suma.js
+++ suma.js
@@ -3,19 +3,4 @@ function suma(a, b) {
     return a + b;
 }
 
-function manejarClick() {
-    const v1 = document.getElementById('v1');
-    const v2 = document.getElementById('v2');
-    const sumar = document.getElementById('sumar');
-
-    sumar.addEventListener('click', () => {
-        let resultado = suma(Number(v1.value), Number(v2.value));
-        document.getElementById('valor').innerHTML = resultado;
-    });
-}
-
-if (typeof window !== 'undefined') {
-    manejarClick();
-}
-
-module.exports = { suma, manejarClick };
+module.exports = { suma };
